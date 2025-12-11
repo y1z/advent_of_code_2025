04601e4ac91acee1fe50c366d216b5866c6f9f81
Yhaliff <whyis2cool@gmail.com>
2025-12-11 12:52:14 -0600

    Start working on day3

---
+++ dayz/day3.java
@@ -0,0 +1,17 @@
+package dayz;
+
+import Utility.Util;
+
+// https://adventofcode.com/2025/day/3
+public final class day3 {
+
+    public static long part_1()
+    {
+        long result = 0;
+
+
+        return result;
+    }
+
+    
+}
