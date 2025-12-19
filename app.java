6f8e4a3b40aa56e9c59378a03701080defac5f77
Yhaliff <whyis2cool@gmail.com>
2025-12-18 23:47:37 -0600

    Modify main.

--- app.java
+++ app.java
@@ -27,6 +27,7 @@ final class App
         // format_day_solution(() -> {day5.part_2();}, 5,2);
 
         format_day_solution(() -> { day6.part_1(); }, 6, 1);
+        format_day_solution(() -> { day7.part_1(); }, 7, 1);
     }
 
     public static void format_day_solution(PrintDayCaller p, int day_num,
