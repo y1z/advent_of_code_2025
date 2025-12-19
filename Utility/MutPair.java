37ba435dab2a89dd25f1880fe32af302c6c0909a
Yhaliff <whyis2cool@gmail.com>
2025-12-18 23:45:59 -0600

    Add Utility class MutPair

---
+++ Utility/MutPair.java
@@ -0,0 +1,13 @@
+package Utility;
+
+public final class MutPair<K, V>
+{
+    public K first;
+    public V second;
+
+    public MutPair(K _first, V _second)
+    {
+        this.first  = _first;
+        this.second = _second;
+    }
+}
